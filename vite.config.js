00f226b5ef23da071a37921f34330cf48b415769
santiagoprietooo <santiago.prieto@davinci.edu.ar>
2025-05-05 16:51:59 -0300

    SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+      hostname: 'https://tarein.netlify.app/'
+    })
+  ]
 })
