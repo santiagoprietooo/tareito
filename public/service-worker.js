bf4e78aa794b7ef975e5819c30b7b4a41b32cafe
santiagoprietooo <santiago.prieto@davinci.edu.ar>
2025-05-11 02:20:45 -0300

    Página de inicio casi terminada y se modificaron las notificaciones. Se agregó un service worker.

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("tarein-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/Icons/Tareín-fondo_transparente.png",
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("push", (event) => {
+    const data = event.data.json();
+    const options = {
+        body: data.task,
+        icon: "/icons/Tareín-fondo_blanco.jpg",
+        badge: "/icons/Tareín-fondo_blanco.jpg"
+    };
+
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
